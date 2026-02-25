7f2e429a1bd55d2e00346ce77c774faf45b26234
Pierre Wilmot <pierre.wilmot@gmail.com>
2026-02-25 14:21:57 +0000

    Update

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,7 +1,9 @@
 
 # Ubuntu fresh install setup script.
-
 sudo apt-get update && sudo apt-get upgrade
+
+# Nvidia driver (RTX3090)
+sudo apt install nvidia-driver-580-open
 
 # Common software
 sudo apt-get install git tig tree cmake build-essential zsh htop emacs ffmpeg terminator openssh-server python3.12-venv sqlite3 tmux cgdb
@@ -19,8 +21,13 @@ echo alias ne=\"emacs -nw\" >> ~/.zshrc
 python3 -m venv .env
 source .env/bin/activate
 pip install --upgrade pip
+
+# Conan
 pip install conan
 conan profile detect
+
+# Pytorch
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu130
 
 # Cuda toolkit
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-ubuntu2404.pin
@@ -28,7 +35,7 @@ sudo mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/12.9.0/local_installers/cuda-repo-ubuntu2404-12-9-local_12.9.0-575.51.03-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu2404-12-9-local_12.9.0-575.51.03-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2404-12-9-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get updatesudo apt-get -y install cuda-toolkit-12-9
+sudo apt-get update && sudo apt-get -y install cuda-toolkit-12-9
 echo "export PATH=/usr/local/cuda/bin:$PATH" >> ~/.zshrc
 
 # Aliases
